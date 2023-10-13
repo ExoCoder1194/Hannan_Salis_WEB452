9f87e7535a3fd488cfc71445e6942ada20b17da7
ExoCoder1194 <118779877+ExoCoder1194@users.noreply.github.com>
2023-10-13 13:48:19 -0400

    Uploaded The next bunch of files

---
+++ Models/ClampModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CLAMP_Assignment1.Models
+{
+    public class Clamp
+    {
+        public int Id { get; set; }
+        public string Material { get; set; }
+        public string Size { get; set; }
+        public string Type { get; set; }
+        public double MaximumOpeningWidth { get; set; }
+        public int LoadCapacity { get; set; }
+        public decimal Price { get; set; }
+        public int CustomerReview { get; set; }
+    }
+
+    public class ClampContext : DbContext
+    {
+        public ClampContext(DbContextOptions<ClampContext> options) : base(options) { }
+
+        public DbSet<Clamp> Clamps { get; set; }
+    }
+
+    public class ClampContextFactory : IDesignTimeDbContextFactory<ClampContext>
+    {
+        public ClampContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ClampContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-CLAMP_Assignment1-BEDC1C97-EBFE-46C8-B024-DE4118C8994E;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new ClampContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using CLAMP_Assignment1.Models;
+
+namespace CLAMP_Assignment1.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ClampContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ClampContext>>()))
+            {
+                // Look for any clamps.
+                if (context.Clamps.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Clamps.AddRange(
+                    new Clamp
+                    {
+                        Material = "metal",
+                        Size = "small",
+                        Type = "C-clamp",
+                        MaximumOpeningWidth = 2, // in inches
+                        LoadCapacity = 20, // in lbs
+                        Price = 5.99m,
+                        CustomerReview = 4
+                    },
+                    new Clamp
+                    {
+                        Material = "plastic",
+                        Size = "medium",
+                        Type = "bar clamp",
+                        MaximumOpeningWidth = 4,
+                        LoadCapacity = 30,
+                        Price = 7.99m,
+                        CustomerReview = 3
+                    },
+                    new Clamp
+                    {
+                        Material = "metal",
+                        Size = "large",
+                        Type = "spring clamp",
+                        MaximumOpeningWidth = 6,
+                        LoadCapacity = 50,
+                        Price = 12.99m,
+                        CustomerReview = 5
+                    },
+                    new Clamp
+                    {
+                        Material = "metal",
+                        Size = "small",
+                        Type = "bar clamp",
+                        MaximumOpeningWidth = 2.5,
+                        LoadCapacity = 25,
+                        Price = 6.49m,
+                        CustomerReview = 3
+                    },
+                    new Clamp
+                    {
+                        Material = "plastic",
+                        Size = "small",
+                        Type = "spring clamp",
+                        MaximumOpeningWidth = 3,
+                        LoadCapacity = 15,
+                        Price = 3.99m,
+                        CustomerReview = 2
+                    },
+                    new Clamp
+                    {
+                        Material = "metal",
+                        Size = "medium",
+                        Type = "C-clamp",
+                        MaximumOpeningWidth = 5,
+                        LoadCapacity = 40,
+                        Price = 10.49m,
+                        CustomerReview = 4
+                    },
+                    new Clamp
+                    {
+                        Material = "metal",
+                        Size = "large",
+                        Type = "bar clamp",
+                        MaximumOpeningWidth = 7,
+                        LoadCapacity = 60,
+                        Price = 15.99m,
+                        CustomerReview = 5
+                    },
+                    new Clamp
+                    {
+                        Material = "plastic",
+                        Size = "medium",
+                        Type = "spring clamp",
+                        MaximumOpeningWidth = 4.5,
+                        LoadCapacity = 35,
+                        Price = 8.49m,
+                        CustomerReview = 4
+                    },
+                    new Clamp
+                    {
+                        Material = "wood",
+                        Size = "medium",
+                        Type = "hand screw clamp",
+                        MaximumOpeningWidth = 3.5,
+                        LoadCapacity = 30,
+                        Price = 9.49m,
+                        CustomerReview = 4
+                    },
+                    new Clamp
+                    {
+                        Material = "metal",
+                        Size = "extra large",
+                        Type = "pipe clamp",
+                        MaximumOpeningWidth = 8,
+                        LoadCapacity = 70,
+                        Price = 17.99m,
+                        CustomerReview = 5
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+
+        internal static void Initialize(ClampContext dbContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
